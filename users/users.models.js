2136fc5aa36e654b6b7ec2eb700cb5141d30ed09
Victor Aliche <alichevictor@gmail.com>
2023-09-08 08:24:02 +0100

    account service

--- users/users.models.js
+++ users/users.models.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import  crypto  from 'crypto';
+
 
 const UserSchema = new mongoose.Schema(
   {
@@ -20,6 +22,19 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+
+    active: {
+      type: Boolean,
+      default: true,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
   },
   { timestamps: true }
 );
@@ -32,4 +47,28 @@ UserSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+UserSchema.pre('save', function(next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+UserSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; //expires in 10 min
+
+  return resetToken;
+};
+
+
 export default mongoose.model("User", UserSchema);
